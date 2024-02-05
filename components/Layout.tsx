db12eaabdaf14e628d38040d61b26027c9451b03
Abhijeet Sharma <abhijeet.vishu@gmail.com>
2024-02-05 20:03:29 +0530

    initial setup and layout

---
+++ components/Layout.tsx
@@ -0,0 +1,26 @@
+import React, { ReactNode } from 'react';
+
+import Sidebar from '@/components/layout/Sidebar';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = props => {
+  const { children } = props;
+
+  return (
+    <div className="h-screen bg-black">
+      <div className="container h-full mx-auto xl:px-30 max-w-6xl">
+        <div className="grid grid-cols-4 h-full">
+          <Sidebar />
+          <div className="col-span-3 lg:col-span-2 border-x-[1px] border-neutral-800">
+            {children}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
